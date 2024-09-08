cc77cd36c8098bf1fc52a7baee5544529f01e3f6
Riskerman21 <abdallah33319@gmail.com>
2024-09-08 11:32:42 +1000

    reversing failed hidden test

--- src/farm/inventory/BasicInventory.java
+++ src/farm/inventory/BasicInventory.java
@@ -43,12 +43,8 @@ public class BasicInventory implements Inventory {
                            Quality quality,
                            int quantity)
             throws InvalidStockRequestException {
-        if (quantity != 1) {
-            throw new InvalidStockRequestException(
+        throw new InvalidStockRequestException(
                     "Current inventory is not fancy enough. Please supply products one at a time.");
-        } else {
-            addProduct(barcode, quality);
-        }
     }
 
     /**
